01a855581964929442122c6dda1cda411c1aa981
infinith4 <infinith4@gmail.com>
2022-07-27 22:30:27 +0900

    create tag

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
-
+import Navbar from './components/Navbar'
+import Main from './components/Main'
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Main />
     </div>
   );
 }
