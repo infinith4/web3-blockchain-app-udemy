33cb1b43c3e4babe453ad31abfbdc0fecbe47d27
infinith4 <infinith4@gmail.com>
2022-07-28 22:18:47 +0900

    Update Main.js

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div className='mainContainer'>
+
+    <div className='cryptContainer'>
+      <h1 className='title'>Crypt Card</h1>
+      <button type='button'>
+        <p className="buttonText">connect wallet</p>
+      </button>
+    </div>
+    <div className='inputContainer'>
+    <input type='text' placeholder='address' name="addressTo" />
+      <input type='number' placeholder='address' name="amount" step="0.0001" />
+    <button type='button'>send</button>
+    </div>
+    </div>
   )
 }
 
