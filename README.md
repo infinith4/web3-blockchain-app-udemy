1c0bd312bfe9444074071dc2d968254e44648cab
infinith4 <infinith4@gmail.com>
2022-07-28 22:39:03 +0900

    structure

--- README.md
+++ README.md
@@ -16,7 +16,8 @@ npm init -y
 ```
 npm start
 ```
-
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
 
 ```
 cd smart_contract
---
+++ smart_contract/contracts/Transactions.sol
@@ -0,0 +1,11 @@
+// smart contract
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract Transactions {
+    struct TransferStruct {
+        address sender;
+        address receiver;
+        uint amount;
+    }
+}
