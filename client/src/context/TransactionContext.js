4cf6f64ff13ce48c7270598e804f354121df0e15
infinith4 <infinith4@gmail.com>
2022-08-02 22:58:30 +0900

    mod

--- client/src/context/TransactionContext.js
+++ client/src/context/TransactionContext.js
@@ -1,16 +1,18 @@
 import { ethers } from "ethers"
+import { createContext, useEffect } from "react";
 import { contractABI, contractAddress } from "../utils/connect";
 
 
-export const transactionContext = createContext();
+export const TransactionContext = createContext();
 
+const { ethereum } = window.ethereum;
 
 //get smartcontract
 
 const getSmartContract = () => {
     // A Web3Provider wraps a standard Web3 provider, which is
     // what MetaMask injects as window.ethereum into each page
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const provider = new ethers.providers.Web3Provider(ethereum)
     // MetaMask requires requesting permission to connect users accounts
     //await provider.send("eth_requestAccounts", []);
 
@@ -28,5 +30,28 @@ const getSmartContract = () => {
 }
 
 export const TransactionProvider = ({children}) => {
-    return (<TransactionContext.Provider value={{name: "shincode" }}>{children}</TransactionContext.Provider>)
+    const [currentAccount, setCurrentAccount] = useState("");
+
+    const checkMetamaskWalletConnected = async () => {
+        if(!ethereum) return alert("install metamask");
+        const accounts = await ethereum.request({method: "eth_accounts"});
+        console.log(accounts);
+    }
+
+    //メタマスクウォレットと連携する
+    const connectWallet = async () => {
+
+        if(!ethereum) return alert("install metamask");
+        //メタマスクを持っていれば接続を開始する
+        const accounts = await ethereum.request({method: 'eth_requestAccounts'});
+        console.log(accounts[0]);
+        
+        setCurrentAccount(accounts[0]);
+    }
+
+    useEffect(() => {
+        checkMetamaskWalletConnected();
+    },[]);
+
+    return (<TransactionContext.Provider value={{name: "shincode" }}>{children}</TransactionContext.Provider>);
 };
